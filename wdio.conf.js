a921072460ef675cc9cc6b02cf47f8a68941b49d
skapoor <skapoor@affinionds.com>
2020-10-07 12:13:01 +0530

    POC on cucumber

--- wdio.conf.js
+++ wdio.conf.js
@@ -68,7 +68,7 @@ exports.config = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'info',
+    logLevel: 'error',
     //
     // Set specific log levels per logger
     // loggers:
@@ -131,6 +131,9 @@ exports.config = {
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
     reporters: ['spec'],
+    reporters: [['allure', {
+        outputDir: './allure-results',
+      }]],
     
     //
     // Options to be passed to Mocha.
